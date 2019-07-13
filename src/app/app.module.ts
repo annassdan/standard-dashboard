5cfeb36860fb972353ea71518d41381f8e3a6237
annassdan <annassdan@gmail.com>
2019-07-13 19:11:59 +0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import {TheDashboardComponent} from './ideas/the-dashboard/the-dashboard.component';
+import { AlatTanngkapComponent } from './ideas/master/alat-tanngkap/alat-tanngkap.component';
+import {DeviceDetectorModule} from 'ngx-device-detector';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MainStateService} from './shared/services/main-state.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheDashboardComponent,
+    AlatTanngkapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    /* dari angular cdk */
+    LayoutModule,
+
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatListModule,
+    MatProgressBarModule,
+
+    DeviceDetectorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,3 @@
+
+
+export const MAX_WIDTH = 890;
